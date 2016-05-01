a5333f3f1ba78bef34a0793295c67a20c84fdf2e
Chance Smith <chance@sodiumhalogen.com>
2016-05-01 15:45:21 -0500

    add user cound to dashboard

--- client/templates/dashboard/dashboard.js
+++ client/templates/dashboard/dashboard.js
@@ -1,3 +1,20 @@
+// client: subscribe to the count for posts
+Tracker.autorun(function () {
+  Meteor.subscribe("postCounts");
+  Meteor.subscribe("userCounts");
+});
+
 Template.dashboard.rendered = function() {
 
 };
+
+Template.dashboard.helpers({
+	postsCount: function(){
+		var postsCount = Stats.findOne({_id: "postCounts"})
+		return postsCount.count
+	},
+	usersCount: function(){
+		var usersCount = Stats.findOne({_id: "userCounts"})
+		return usersCount.count
+	}
+});
--- server/publications/userCounts.js
+++ server/publications/userCounts.js
@@ -1,24 +1,24 @@
 // server: publish the current size of your post collection
-Meteor.publish("stats", function () {
+Meteor.publish("userCounts", function () {
   var self = this;
   var count = 0;
   var initializing = true;
 
-  var handle = Posts.find().observeChanges({
+  var handle = Meteor.users.find().observeChanges({
     added: function (id) {
       count++;
       if (!initializing)
-        self.changed("stats", 'postCounts', {count: count});
+        self.changed("stats", 'userCounts', {count: count});
     },
     removed: function (id) {
       count--;
-      self.changed("stats", 'postCounts', {count: count});
+      self.changed("stats", 'userCounts', {count: count});
     }
 
   });
 
   initializing = false;
-  self.added("stats", 'postCounts', {count: count});
+  self.added("stats", 'userCounts', {count: count});
   self.ready();
 
   self.onStop(function () {
